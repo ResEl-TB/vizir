6e9464e0235b9b990ded6b9d6830eed6dc4af8ed
bsomers <benjamin.somers0@gmail.com>
2018-05-01 20:28:52 +0200

    Add unstable yet promising deep list-config checker

--- validator.py
+++ validator.py
@@ -11,8 +11,10 @@ def _(*types):
 
 _str = _(str)
 _str.__name__ = "_str"
-def _str_list(x):
-    return bool(x) and all(_str(e) for e in x)
+_list = _(list)
+_list.__name__ = "_list"
+_dict = _(dict)
+_dict.__name__ = "_dict"
 
 def solve(dict, branch, index = None):
     sub = reduce(operator.getitem, branch, dict)
@@ -29,23 +31,32 @@ def explore(dic, branch, named_branch, f, g):
         fields |= {child}
     g(branch, named_branch, fields)
 
+files_policy = {"_error": {"@", "plus", "to"},
+               "minus": [_str],
+               "plus": [_str],
+               "to": _str}
+
 policies = {"*": {"_error": {"@", "docs", "locales"},
                   "_warning": {"authors", "code", "copyright", "release", "version"},
                   "authors": _str, # no list?
-                  "code": {"*": {"_error": {"@", "plus"},
+                  "code": {"*": {"_error": {"@"},
                                  "_warning": {"templates"},
+                                 "files": [files_policy],
                                  "from": _str,
-                                 "minus": _str_list,
-                                 "plus": _str_list,
-                                 "templates": _str_list}},
+                                 "templates": [_str]}},
                   "copyright": _str,
-                  "docs": {"_error": {"@", "plus"},
-                           "from": _str,
-                           "minus": _str_list,
-                           "plus": _str_list},
-                  "locales": _str_list,
+                  "docs": {"_error": {"@"},
+                           "files": [files_policy],
+                           "from": _str},
+                  "locales": [_str],
                   "release": _str,
                   "version": _str}}
+
+def final(rule, value, index, named_branch):
+    c = callable(rule)
+    if c == _dict(value) or c and not rule(value):
+        print("ERROR: {} failed the check {} in branch {}".format(index, rule.__name__ if c else "_dict", ":".join(named_branch)))
+        sys.exit(1)
 
 def check_policies(branch, named_branch, index, value):
     global policies
@@ -57,10 +68,20 @@ def check_policies(branch, named_branch, index, value):
         elif "_warning" in sub and "@" in sub["_warning"]:
             print("WARNING: Unexpected field: {} in branch {}".format(index, ":".join(named_branch)))
     if ref in sub:
-        c = callable(sub[ref])
-        if c == isinstance(value, dict) or c and not sub[ref](value):
-            print("ERROR: {} failed the check {} in branch {}".format(index, sub[ref].__name__ if c else "_dict", ":".join(named_branch)))
-            sys.exit(1)
+        if _list(sub[ref]):
+            if _list(value):
+                for i in range(len(value)):
+                    if _dict(value[i]):
+                        explore(value[i], branch + [index, 0], named_branch + [index, "#{}".format(i)], check_policies, check_missing)
+                    else:
+                        final(sub[ref][0], value[i], index, named_branch + [index, "#{}".format(i)])
+            else:
+                if _dict(value):
+                    explore(value, branch + [index, 0], named_branch + [index, "#"], check_policies, check_missing)
+                else:
+                    final(sub[ref][0], value, index, named_branch + [index, "#"])
+        else:
+            final(sub[ref], value, index, named_branch)
     return ref
 
 def missing(fields, policy_fields):
